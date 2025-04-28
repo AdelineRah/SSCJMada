9bbaeaae6a449b4e3044064d732df9a925245d1b
AdelineRah <adelinerahel@gmail.com>
2025-04-28 17:52:56 +0300

    modification

---
+++ inc/footer.php
@@ -0,0 +1,3 @@
+footer>
+        <p>&copy;copyright-2025.Tous droits réservés au droit d'auteur.</p>
+    </footer>
---
+++ inc/header.php
@@ -0,0 +1,35 @@
+ <header>
+        <nav>
+            <div class="logo">
+                <img src="images/sscj_logo_4c_francais_pays-madagascar1.png" alt="">
+            </div>
+                <ul>
+                    <li><a href="accueil">Accueil</a></li>
+                    <li>
+                        <a href="a-propos">À propos</a>
+                        <ul class="dropdown">
+                            <li><a href="historique2.html">Historique et missionnaires</a></li>
+                            <li><a href="Communautés.html">Les communautés</a></li>
+                            <li><a href="Associés.html">Les associés</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="mission">Mission</a>
+                        <ul class="dropdown">
+                            <li><a href="Etapes de formation.html">Formation</a></li>
+                            <li><a href="critères .html">Critères</a></li>
+                            <li><a href="Activité.html">Activités</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="mission">Actualités</a>
+                        <ul class="dropdown">
+                            <li><a href="témoignages">Témoignages</a></li>
+                            <li><a href="Evènements .html">Evènements</a></li>
+                        </ul>
+                    </li>
+                   
+                </ul>
+               <button class="btn2"><a href="donation">Donation</a></button>
+            </nav>
+    </header>
